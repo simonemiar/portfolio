c2b1dcb73bd1d588324c13f09d74a8072e7b7f3f
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-01 15:43:46 +0200

    menu is working

---
+++ burger.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+document.addEventListener('DOMContentLoaded', nav)
+function nav(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+    })
+}
