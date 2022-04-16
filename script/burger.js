12d534a0eb96d61d6149107891e5dedc3048c67c
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-16 17:40:09 +0200

    mobil menu fixed, desktop needs styling

--- script/burger.js
+++ script/burger.js
@@ -2,6 +2,29 @@
 
 console.log("hellow burger");
 
+const burgerButton = document.querySelector(".burgerButton");
+const streg1 = document.querySelector(".streg1");
+const streg2 = document.querySelector(".streg2");
+const streg3 = document.querySelector(".streg3");
+const navbarLinks = document.querySelector(".navbarLinks");
+
+// Når HTML og CSS er loaded startes sidenVises
+window.addEventListener("load", sidenVises);
+
+// Når #burger_btn er "clicked" startes openMenu
+function sidenVises() {
+  burgerButton.addEventListener("click", open);
+}
+
+function open() {
+  console.log("open menu");
+  streg1.classList.toggle("aktiv");
+  streg2.classList.toggle("aktiv");
+  streg3.classList.toggle("aktiv");
+  navbarLinks.classList.toggle("aktiv");
+}
+
+
 // document.addEventListener('DOMContentLoaded', nav)
 // function nav(){
 //     const burger = document.querySelector('.burger');
