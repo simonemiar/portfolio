8ad1b1bb9ff830219a15ab6d3d307bfa08c714ad
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-18 16:40:02 +0200

    button link styling fixed

--- script/animation.js
+++ script/animation.js
@@ -1,1 +1,9 @@
 "use strict"
+
+let h1 = document.querySelector("h1");
+
+gsap.from("h1", {opacity: 0, stagger: 2, duration: 1, y: -50})
+
+document.querySelector("#cta").addEventListener('click',  () => {
+    h1.reversed() ? h1.play () : h1.reverse();
+})
