359902b244a26d96543cdeceddc9b3df1db2e1bc
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-26 19:25:04 +0200

    branch out

--- script/animation.js
+++ script/animation.js
@@ -1,16 +1,20 @@
 "use strict"
 
 console.log("hellow animation");
+window.onload = function(){
+    //hide the preloader
+    document.querySelector(".preloader").style.display = "none";
+}
 
-// Loading animation
+// Loading page animation
 gsap.to('.layer-1', {y: '-100vh', delay: .5});
 gsap.to('.layer-2', {y: '-100vh', delay: .7});
 gsap.to('.layer-3', {y: '-100vh', delay: .9});
 gsap.to('.overlay', {y: '-100vh', delay: 1.5});
 
-// Loading animation done
 
 
+// BackToTop animated button
 
 
 // let h1 = document.querySelector("h1");
