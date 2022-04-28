687c24725a6c8ff938f53f587d79bd7d15196e35
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-28 21:21:32 +0200

    font page animation fixed

--- script/animation.js
+++ script/animation.js
@@ -8,8 +8,30 @@ gsap.to('.layer-2', {y: '-100vh', delay: .5});
 gsap.to('.layer-3', {y: '-100vh', delay: .7});
 gsap.to('.overlay', {y: '-100vh', delay: 1});
 
+// font page animation - first section 
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
 
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
 
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
 // BackToTop animated button
 
 
