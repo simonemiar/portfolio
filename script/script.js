2a2497ebf9be0fb8b77f7c46fca8a5b977bf57b2
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-05-02 01:11:46 +0200

    process images updated

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 "use strict";
 
 // INTERSECTION CODE 
-
 window.addEventListener('DOMContentLoaded', setup); 
+
+const paras = document.querySelectorAll('section');
 
 function setup() {
     const options = {
@@ -12,7 +13,7 @@ function setup() {
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if(entry.isIntersecting) {
-                entry.target.classList.add('show'); // here we add the class
+                entry.target.classList.add('show', entry.isIntersecting); // here we add the class
                 observer.unobserve(entry.target); // adds unobserve 
             } else {
                 return;
@@ -20,6 +21,22 @@ function setup() {
         })
     }, options);
 
-    const paras = document.querySelectorAll('section');
+
     paras.forEach(p => observer.observe(p));
 }
+
+
+// const paras = document.querySelectorAll('section');
+
+// const observer = new IntersectionObserver(entries =>{
+//     entries.forEach(entry => {
+//         entry.target.classList.toggle("show", entry.isIntersecting)
+//     } )
+//     // console.log(entries)
+// }, {
+//     threshold: 1 // animation begins when element is some amount visible, default: 0 
+// })
+
+// paras.forEach( card =>{
+//     observer.observe(john[0])
+// })
