fa46edee34c7fa074bf5eb79c8dbc75b96b22356
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-05-01 23:51:22 +0200

    intersection onbserver done

--- script/script.js
+++ script/script.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.log("hellow script");
+// INTERSECTION CODE 
+
+window.addEventListener('DOMContentLoaded', setup); 
+
+function setup() {
+    const options = {
+        rootMargin: '0px 0px -120px 0px'
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show'); // here we add the class
+                observer.unobserve(entry.target); // adds unobserve 
+            } else {
+                return;
+            }
+        })
+    }, options);
+
+    const paras = document.querySelectorAll('section');
+    paras.forEach(p => observer.observe(p));
+}
